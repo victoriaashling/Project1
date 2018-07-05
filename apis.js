e22d729493a17ec1b5e4ffc81d63c91249ab170c
victoriaashling <victoria.ashling@gmail.com>
2018-07-05 10:26:36 -0400

    one bit fixed, a bit more to go

--- apis.js
+++ apis.js
@@ -37,7 +37,7 @@ $("#log-out-button").click(function() {
     })
 })
 
-$(".fb-login-button").click(function() {
+$("#fb-login").click(function() {
     FB.logout(function(response){
         checkLoginState();
     })
