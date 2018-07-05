d7d8256c3c322bc0bf9c0bfe13ec6de0a84a7c60
victoriaashling <victoria.ashling@gmail.com>
2018-07-05 14:30:01 -0400

    trying some styled buttons ... style needs much work

--- apis.js
+++ apis.js
@@ -42,3 +42,28 @@ $("#fb-login").click(function() {
         checkLoginState();
     })
 })
+
+
+
+
+
+
+    
+function openExperienceType(event, experience) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(experience).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+
+  document.getElementById("default-open").click();
