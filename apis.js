0e8041a764665d8e104dfbd30eaf1f86551a1633
victoriaashling <victoria.ashling@gmail.com>
2018-07-05 18:33:48 -0400

    still not working

--- apis.js
+++ apis.js
@@ -1,46 +1,93 @@
+$(document).ready(function() {
 
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-}
-
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else {
-      // The person is not logged into your app or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+        });
     }
-}
-
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-      console.log(response);
-    });
-}
-
-$("#log-out-button").click(function() {
-    FB.logout(function(response){
+    
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+          // Logged into your app and Facebook.
+          testAPI();
+        } else {
+          // The person is not logged into your app or we are unable to tell.
+          document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+        }
+    }
+    
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+          console.log(response);
+        });
+    }
+    
+    $("#log-out-button").click(function() {
+        FB.logout(function(response){
+            checkLoginState();
+        })
+    })
+    
+    $(".fb-login-button").click(function() {
         checkLoginState();
     })
-})
+    
 
-$(".fb-login-button").click(function() {
-    checkLoginState();
-})
+
+
+// function checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//       statusChangeCallback(response);
+//     });
+// }
+
+// function statusChangeCallback(response) {
+//     console.log('statusChangeCallback');
+//     console.log(response);
+//     // The response object is returned with a status field that lets the
+//     // app know the current login status of the person.
+//     // Full docs on the response object can be found in the documentation
+//     // for FB.getLoginStatus().
+//     if (response.status === 'connected') {
+//       // Logged into your app and Facebook.
+//       testAPI();
+//     } else {
+//       // The person is not logged into your app or we are unable to tell.
+//       document.getElementById('status').innerHTML = 'Please log ' +
+//         'into this app.';
+//     }
+// }
+
+// function testAPI() {
+//     console.log('Welcome!  Fetching your information.... ');
+//     FB.api('/me', function(response) {
+//       console.log('Successful login for: ' + response.name);
+//       document.getElementById('status').innerHTML =
+//         'Thanks for logging in, ' + response.name + '!';
+//       console.log(response);
+//     });
+// }
+
+// $("#log-out-button").click(function() {
+//     FB.logout(function(response){
+//         checkLoginState();
+//     })
+// })
+
+// $(".fb-login-button").click(function() {
+//     checkLoginState();
+// })
 
 
 
