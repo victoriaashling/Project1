26620751da99b0f03ffef070577e3671cc230875
victoriaashling <victoria.ashling@gmail.com>
2018-07-05 15:57:09 -0400

    buttons

--- apis.js
+++ apis.js
@@ -37,9 +37,9 @@ $("#log-out-button").click(function() {
     })
 })
 
-$("#fb-login").click(function() {
-    checkLoginState();
-})
+// $(".fb-login-button").click(function() {
+//     checkLoginState();
+// })
 
 
 
