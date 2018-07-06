c90803e592012ef6b903cd44b82a689cf2d542ca
victoriaashling <victoria.ashling@gmail.com>
2018-07-05 20:40:48 -0400

    1.18

--- apis.js
+++ apis.js
@@ -8,15 +8,9 @@ function checkLoginState() {
 function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
       testAPI();
     } else {
-      // The person is not logged into your app or we are unable to tell.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     }
@@ -38,77 +32,6 @@ $("#log-out-button").click(function() {
     })
 })
 
-$(".fb-login-button").click(function() {
-    checkLoginState();
-})
-
-
-
-
-// function checkLoginState() {
-//     FB.getLoginStatus(function(response) {
-//       statusChangeCallback(response);
-//     });
-// }
-
-// function statusChangeCallback(response) {
-//     console.log('statusChangeCallback');
-//     console.log(response);
-//     // The response object is returned with a status field that lets the
-//     // app know the current login status of the person.
-//     // Full docs on the response object can be found in the documentation
-//     // for FB.getLoginStatus().
-//     if (response.status === 'connected') {
-//       // Logged into your app and Facebook.
-//       testAPI();
-//     } else {
-//       // The person is not logged into your app or we are unable to tell.
-//       document.getElementById('status').innerHTML = 'Please log ' +
-//         'into this app.';
-//     }
-// }
-
-// function testAPI() {
-//     console.log('Welcome!  Fetching your information.... ');
-//     FB.api('/me', function(response) {
-//       console.log('Successful login for: ' + response.name);
-//       document.getElementById('status').innerHTML =
-//         'Thanks for logging in, ' + response.name + '!';
-//       console.log(response);
-//     });
-// }
-
-// $("#log-out-button").click(function() {
-//     FB.logout(function(response){
-//         checkLoginState();
-//     })
-// })
-
 // $(".fb-login-button").click(function() {
 //     checkLoginState();
 // })
-
-
-
-
-
-
-    
-// function openExperienceType(event, experience) {
-//     var i, tabcontent, tablinks;
-
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = "none";
-//     }
-
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace("active", "");
-//     }
-
-//     document.getElementById(experience).style.display = "block";
-//     event.currentTarget.className += " active";
-//   }
-
-// document.getElementById("default-open").click();
